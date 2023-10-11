2b0e6467fea4d2eca36d34c52593697333cf8fb4
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-11 00:02:51 -0300

    feat: create user route

--- src/main/java/br/com/pdrmenezes/todo/user/UserController.java
+++ src/main/java/br/com/pdrmenezes/todo/user/UserController.java
@@ -1,5 +1,16 @@
 package br.com.pdrmenezes.todo.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
-  
+  @PostMapping("/create")
+  public String create(@RequestBody UserModel userModel){
+    System.out.println(userModel);
+    return userModel.username;
+    }
 }
