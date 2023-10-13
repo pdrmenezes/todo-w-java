0ee9df8b942fded5afaabf3dbc3b5679af49fa95
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-13 13:22:51 -0300

    feat:map user model to 'app_users' on db + add autogenerated uuid value

--- src/main/java/br/com/pdrmenezes/todo/user/UserModel.java
+++ src/main/java/br/com/pdrmenezes/todo/user/UserModel.java
@@ -1,9 +1,18 @@
 package br.com.pdrmenezes.todo.user;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "app_users")
 public class UserModel {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
   private String username;
   private String name;
   private String password;
