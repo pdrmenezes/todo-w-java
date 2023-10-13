27ae50955418f0bd33f355eca029932bc76b29c9
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-13 13:27:24 -0300

    feat:add createdAt property and named user entity

--- src/main/java/br/com/pdrmenezes/todo/user/UserModel.java
+++ src/main/java/br/com/pdrmenezes/todo/user/UserModel.java
@@ -1,6 +1,9 @@
 package br.com.pdrmenezes.todo.user;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,8 +16,13 @@ public class UserModel {
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
+  
+  // if we want to map a name different than the property name inside the db we can use the @Column annotation to give it a new name
+  // e.g.: @Column(name="alias")
   private String username;
   private String name;
   private String password;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
   
 }
