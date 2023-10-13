6a1d9b38b60e3eba50f7a191ad7f04043555e336
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-13 13:46:45 -0300

    implementing the user interface to use its methods w the db + adjusting the return of create method

--- src/main/java/br/com/pdrmenezes/todo/user/UserController.java
+++ src/main/java/br/com/pdrmenezes/todo/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.pdrmenezes.todo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+  
+  // lets spring intantiate the repository when needed instead of initializing it manually
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/create")
-  public String create(@RequestBody UserModel userModel){
-    System.out.println(userModel);
-    return "new username: " + userModel.getUsername();
+  public UserModel create(@RequestBody UserModel userModel){
+    // now with the userRepository extending the jpa interface, we can access all of its methods (count, delete, find, findById, save...)
+    // this.userRepository.
+    var createdUser = this.userRepository.save(userModel);
+    return createdUser;
+    // System.out.println(userModel);
+    // return "new username: " + userModel.getUsername();
     }
 }
