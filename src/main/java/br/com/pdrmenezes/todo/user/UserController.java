ed1856a0566b8c4ab435ec295c693abdc7affd08
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-13 14:45:43 -0300

    fix:include success status code 201

--- src/main/java/br/com/pdrmenezes/todo/user/UserController.java
+++ src/main/java/br/com/pdrmenezes/todo/user/UserController.java
@@ -23,13 +23,13 @@ public class UserController {
     if(user != null){
       // we can pass a status code number directly or if we're not sure, we can user HttpStatus to select the correct status code
       // and we can pass a body
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado.");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists.");
     }
 
     // now with the userRepository extending the jpa interface, we can access all of its methods (count, delete, find, findById, save...)
     // this.userRepository.
     var createdUser = this.userRepository.save(userModel);
-    return ResponseEntity.status(200).body(createdUser);
+    return ResponseEntity.status(201).body(createdUser);
     // System.out.println(userModel);
     // return "new username: " + userModel.getUsername();
     }
