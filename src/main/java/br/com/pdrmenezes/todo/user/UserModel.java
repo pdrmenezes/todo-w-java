6b785017e8315cf5f51fc0b61a9db42dd09148c8
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-13 13:59:37 -0300

    make the username a unique key

--- src/main/java/br/com/pdrmenezes/todo/user/UserModel.java
+++ src/main/java/br/com/pdrmenezes/todo/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,6 +20,8 @@ public class UserModel {
   
   // if we want to map a name different than the property name inside the db we can use the @Column annotation to give it a new name
   // e.g.: @Column(name="alias")
+  // the "unique" makes it so it errors when we try to create another user with an already existing username
+  @Column(unique = true)
   private String username;
   private String name;
   private String password;
