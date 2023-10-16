8744a49ecf94a9b81033001345383081b583a447
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-16 14:06:45 -0300

    feat:create task table

---
+++ src/main/java/br/com/pdrmenezes/todo/task/TaskModel.java
@@ -0,0 +1,32 @@
+package br.com.pdrmenezes.todo.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "app_tasks")
+public class TaskModel {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  @Column(length = 50)
+  private String title;
+  private String description;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  private UUID userId;
+
+}
