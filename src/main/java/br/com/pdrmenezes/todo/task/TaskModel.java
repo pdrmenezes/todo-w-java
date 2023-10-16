8a0ca8ec316109ee60c7bc5fd433e58260101d5d
pdrmenezes <pdrmenezes1@gmail.com>
2023-10-16 19:53:45 -0300

    fix:typo

--- src/main/java/br/com/pdrmenezes/todo/task/TaskModel.java
+++ src/main/java/br/com/pdrmenezes/todo/task/TaskModel.java
@@ -31,7 +31,7 @@ public class TaskModel {
 
   public void setTitle(String title) throws Exception {
     if (title.length() > 50) {
-      throw new Exception("Title may have up to 50 characters.");
+      throw new Exception("Titles may have up to 50 characters.");
     } else {
       this.title = title;
     }
